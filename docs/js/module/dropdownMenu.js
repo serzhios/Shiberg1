f03a11c5614a9daff9e1fe526c34696dcc52478c
Sergey Rumyancev <studiors39@gmail.com>
2024-11-04 20:52:19 +0200

    главная страница

---
+++ docs/js/module/dropdownMenu.js
@@ -0,0 +1,23 @@
+
+export function dropdownMenu(){
+    if(document.querySelectorAll('.dropdown-section ')){
+        const logo = document.querySelector('.logo-img');
+        let isLogoBig = false;
+        document.getElementById('menuButton').addEventListener('click', function() {
+            const menu = document.getElementById('menu');
+
+            // const
+            if (menu.classList.contains('hidden') && !isLogoBig) {
+                logo.style.width = '203px';
+                logo.style.height ='36px';
+                isLogoBig = true;
+                menu.classList.remove('hidden');
+            } else {
+                logo.style.width = '107px';
+                logo.style.height ='19px';
+                isLogoBig = false;
+                menu.classList.add('hidden');
+            }
+        });
+}
+}
